12831d3def4cbe00b59de98b58b4aefd6e820408
KNPetrovskiy <knpetrovskiy@stud.kpfu.ru>
2025-07-23 18:41:32 +0300

    Add ym

--- app.vue
+++ app.vue
@@ -1,3 +1,8 @@
 <template>
 	<NuxtPage />
+	<noscript>
+		<div>
+			<img src="https://mc.yandex.ru/watch/103438403" style="position:absolute; left:-9999px;" alt="" />
+		</div>
+	</noscript>
 </template>
